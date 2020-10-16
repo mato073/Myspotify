1ecc013e6f9e68204fd6f6f8589d29313b892f89
Gaspard Mater <gaspard.mater@epitech.eu>
2020-10-16 21:38:52 +0200

    [ADD] hash.js file to Get the hash of the url [MOD] App.js call login spotify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
+import hash from "./hash.js";
 import './App.css';
 
-function App() {
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const clientId = "bf5aba2f7f5b4efe956739f92460473d";
+const redirectUri = "http://localhost:8888/callback";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state"
+];
+
+window.location.hash = "";
+
+class App extends Component {
+
+  componentDidMount() {
+    let _token = hash.access_token;
+    if (_token) {
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+  render () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!this.setState.token && (<a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>)}
       </header>
     </div>
   );
 }
+}
 
 export default App;
+
