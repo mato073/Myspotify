f3c544d1807a202d3a440dfea9ad6af48243548f
Gaspard Mater <gaspard.mater@epitech.eu>
2020-10-23 15:37:04 +0200

    [MOD] App component to redirect to component Login

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import hash from "./hash.js";
+import Login from './Components/Login'
 import './App.css';
 
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-const clientId = "bf5aba2f7f5b4efe956739f92460473d";
-const redirectUri = "http://localhost:3000/";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state"
-];
-
-window.location.hash = "";
 
 class App extends Component {
-
-  componentDidMount() {
-    let _token = hash.access_token;
-    if (_token) {
-      this.setState({
-        token: _token
-      });
-    }
-  }
-
   render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        {!this.setState.token && (<a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>)}
-      </header>
-    </div>
+    <Login/>
   );
 }
 }
