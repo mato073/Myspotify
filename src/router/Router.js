a353829213830c5b777b439c62ebb72021dd8900
Gaspard Mater <gaspard.mater@epitech.eu>
2020-10-23 15:38:39 +0200

    [MOD] index.js by ading the Routin [ADD] Routing in router/Router.js

---
+++ src/router/Router.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../Components/Login'
+import Home from '../Components/Home'
+import App from '../App'
+
+class AppRouter extends React.Component {
+    render () {
+        return (
+            <BrowserRouter>
+                <div className="main">
+                    <Switch>
+                        <Route path="/" component={App} exact={true} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/Home" component={Home} />
+                    </Switch>
+                </div>
+
+            </BrowserRouter>
+        );
+    }
+}
+
+export default AppRouter
