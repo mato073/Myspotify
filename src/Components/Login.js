7ac3dd1bb90b4ea7aca8a083f8b81d722aae478e
Gaspard Mater <gaspard.mater@epitech.eu>
2020-10-24 13:15:21 +0200

    [MOD] Login to make it simple + [ADD] pakage material UI

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,44 +1,43 @@
-import React, { Component } from "react";
-import hash from "../hash.js";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import React from "react";
 
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const useStyles = makeStyles({
+  loginBtn: {
+    background: 'green',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    alignItems: 'center',
+  },
+});
 
 const clientId = "bf5aba2f7f5b4efe956739f92460473d";
-const redirectUri = "http://localhost:3000/home";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state"
-];
+const redirectUri = "http://localhost:3000/redirect";
+const authorizeUrl ="https://accounts.spotify.com/authorize"
 
-window.location.hash = "";
+const Login = (props) => {
+  const classes = useStyles();
 
-class Login extends Component {
+  
+  const handleLogin = () => {
+    window.location = `${authorizeUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+  };  
 
-    componentDidMount() {
-        let _token = hash.access_token;
-        if (_token) {
-          this.setState({
-            token: _token
-          });
-        }
-      }
 
-    render () {
-        return (
-         <>
-            <header className="App-header">
-                {!this.setState.token && (<a
-                    className="btn btn--loginApp-link"
-                    href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                    "%20"
-                    )}&response_type=token&show_dialog=true`}
-                >
-                    Login to Spotify
-                </a>)}
-            </header>
-         </>
-        );
-    } 
-}
+  return (
+    <>
+    <h1>Login to Spotify</h1>
+    <div className="login">
+      <Button  onClick={handleLogin} className={classes.loginBtn} type="submit">
+        Login to spotify
+      </Button>
+    </div>
+    </>
+  );
+};
 
-export default Login;
+export default connect()(Login);
