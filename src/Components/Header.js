cf3a7c99ed633c62d278296cb2d04c5033f7cd7e
Archaël_Mestre <pierre.marmai@epitech.eu>
2020-10-30 16:27:29 +0100

    log out button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,82 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import logo from "../images/EpitechMySpotify.svg";
 
 class Header extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            userdata: []
-         };
-      }
+    /*
+    userButton = (event) => {
+        const [anchorEl, setAnchorEl] = React.useState(null);
+        const open = Boolean(anchorEl);
+        const user = JSON.parse(this.props.userdata);
 
-    componentDidMount() {
-        console.log(this.props);
-    }
+        const handleMenu = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
 
-    render () {
+        const handleClose = () => {
+            setAnchorEl(null);
+        };
+
         return (
-        <>
-            <p>Header</p>
-        </>
-    );
+            <div>
+                <Button
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    color="inherit">
+                    <AccountCircle />
+                    <p>{user.display_name}</p>
+                </Button>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}>
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
+            </div>
+        );
+    }
+    */
+
+    render() {
+        return (
+            <AppBar position="static" style={{ backgroundColor: 'black' }}>
+                <Toolbar variant="dense">
+                    <div style={{ maxHeight: '5vh', flexGrow: '1'}}>
+                        <img src={logo} style={{ maxHeight: '5vh'}}></img>
+                    </div>
+                    <Button color="inherit" style={{ float: 'right' }}>
+                        <ExitToAppIcon/>
+                        <p> Log out</p>
+                    </Button>
+                </Toolbar>
+            </AppBar >
+        );
     }
 
 }
 
 const mapStateToProps = (state) => ({
-    token: state.access_token,
-    userdata: state.userdata
+    userdata: state.userdata,
 });
 
 export default connect(mapStateToProps)(Header);
